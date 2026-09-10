72aa346f0add158b6151e5066dd49dbe2e37595e
ludwigleiss14-jpg <ludwig.leiss14@gmail.com>
2026-09-10 22:40:56 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,22 @@
 import os
+from flask import Flask
 import discord
+from threading import Thread
+
+# Flask-Server für Render Port-Binding
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is online!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -33,6 +50,7 @@ class MyClient(discord.Client):
 client = MyClient(intents=intents)
 
 if __name__ == "__main__":
+    keep_alive()
     token = os.getenv("DISCORD_TOKEN")
     if token:
         client.run(token)
